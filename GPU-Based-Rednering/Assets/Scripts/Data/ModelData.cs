3935894055ec451847115aabf26efc551393052d
Alexandros-Koutrakis <koutrakisa@gmail.com>
2025-10-20 16:58:02 +0300

    Read Me

--- GPU-Based-Rednering/Assets/Scripts/Data/ModelData.cs
+++ GPU-Based-Rednering/Assets/Scripts/Data/ModelData.cs
@@ -11,9 +11,9 @@ namespace Rendering.Data
 
         [HideInInspector] public Vertex[] Vertices;
         [HideInInspector] public Triangle[] Triangles;
-        public uint[] TriangleRenderingOrder;//added this 0 for opaques 1 for transparent
+        [HideInInspector] public uint[] TriangleRenderingOrder;//0 for opaques 1 for transparent
         [HideInInspector] public MaterialData[] Materials;
-        [HideInInspector] public Texture2DArray[] TextureArrays;
+        public Texture2DArray[] TextureArrays;
         [HideInInspector] public Cell[] Cells;
         [HideInInspector] public uint[] TriangleIndexes;
 
